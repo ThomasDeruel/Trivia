6237a83c596808a64661c6e6837e2a7db4523600
Thomas Deruel <thomas.deruel@hetic.net>
2018-11-10 17:55:38 +0100

    add resetCategory()

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -10,5 +10,11 @@ class storage {
     localStorage.setItem('storage', JSON.stringify(newdata));
     JSON.parse(localStorage.getItem('storage'));
   }
+  resetCategory() {
+      let storage = this.get();
+      storage.category = '';
+      storage.score = 0;
+      this.set(storage);
+  }
 }
 export default new storage();
